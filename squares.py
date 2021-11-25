2813f79f6eee08ade9422f2cca6bfa2f8f6c080a
Zhiwei PENG <zpsl0428@gmail.com>
2021-11-25 13:35:17 +0000

    add weights

--- squares.py
+++ squares.py
@@ -1,5 +1,5 @@
 """Computation of weighted average of squares."""
-
+import argparse
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
@@ -51,12 +51,11 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
-    
-    result = average_of_squares(numbers, weights)
-    
-    print(result)
+    parser = argparse.ArgumentParser(description="Return the weighted average of a list of values.")
+    parser.add_argument('numbers', type=int, nargs='+')
+    parser.add_argument('--weights', '-w', type=float, nargs='+')
+    arguments = parser.parse_args()
+
+    results = average_of_squares(arguments.numbers, arguments.weights)
+
+    print(results)
